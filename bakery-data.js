617af3e1b2c8229baf400334d5e099d49e33f5dc
Pree Tiamsawate <preetiamsawate@gmail.com>
2021-06-17 17:20:59 +0700

    Bakery and Meal data functions

---
+++ bakery-data.js
@@ -0,0 +1,257 @@
+// Bakery Database
+let Croissants = [
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test2<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+     {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },  
+];
+
+let Originals = [
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+];
+let Snacks = [
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test2<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+     {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },  
+];
+let Sweeties = [
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test2<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+     {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },  
+];
+let Complimentaries = [
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test1<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test2<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+     {
+        imgPath: "./images/v41_21.png",
+        nameHTML: 'Test3<br>Croissant',
+        nameThai: 'ครัวซองค์รสต้นตำหรับ ',
+        price: 45,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },  
+];
+
+// Element Creater================
+const addCards = function(box, Foods) {
+    for (food of Foods) {
+        const cardSmall = document.createElement('div');
+        const foodImg = document.createElement('img');
+        const cardText = document.createElement('div');
+        const foodNameHTML = document.createElement('p');
+        const foodNameThai = document.createElement('span');
+        const foodPrice = document.createElement('span');
+        const ingredientHTML = document.createElement('div');
+        const foodDescription = document.createElement('div');
+        const preservationHTML = document.createElement('div');
+
+        cardSmall.classList.add("card-small", "flex-shrink-0", "mx-2");
+        cardSmall.setAttribute("data-bs-toggle", "modal");
+        cardSmall.setAttribute("data-bs-target", "#modal-bakery");
+        foodImg.classList.add("d-block", "card-img-small");
+        foodImg.setAttribute("src", food.imgPath);
+        cardText.classList.add("card-text", "w-100", "h-auto", "pt-1", "ps-1");
+
+        if (box == croissantBox || box == originalBox) {
+            foodNameHTML.classList.add("card-name-small", "mb-md-1");
+            foodNameThai.classList.add("card-name-thai-small", "text-wrap", "float-start");
+            foodPrice.classList.add("card-price-small", "float-end");
+        } else {
+            foodNameHTML.classList.add("card-name-small-white", "mb-md-1");
+            foodNameThai.classList.add("card-name-thai-small-white", "text-wrap", "float-start");
+            foodPrice.classList.add("card-price-small-white", "float-end");
+        }
+        foodNameHTML.innerHTML = food.nameHTML;
+        foodNameThai.innerText = food.nameThai;
+        foodPrice.innerText = food.price + ",-";
+        ingredientHTML.classList.add("ingredient", "d-none");
+        ingredientHTML.innerHTML = food.ingredientHTML;
+        foodDescription.classList.add("productDescription", "d-none");
+        foodDescription.innerText = food.description;
+        preservationHTML.classList.add("preservation", "d-none");
+        preservationHTML.innerHTML = food.preservationHTML;
+
+        cardText.appendChild(foodNameHTML);
+        cardText.appendChild(foodNameThai);
+        cardText.appendChild(foodPrice);
+        cardSmall.appendChild(foodImg);
+        cardSmall.appendChild(cardText);
+        cardSmall.appendChild(ingredientHTML);
+        cardSmall.appendChild(foodDescription);
+        cardSmall.appendChild(preservationHTML);
+
+        box.appendChild(cardSmall);
+    }
+}
+
+//Create and Add Elements
+
+const croissantBox = document.querySelector('#croissantBox');
+const originalBox = document.querySelector('#orignalBox');
+const snackBox = document.querySelector('#snackBox');
+const sweetyBox = document.querySelector('#sweetyBox');
+const complimentaryBox = document.querySelector('#complimentaryBox');
+addCards(croissantBox, Croissants);
+addCards(originalBox, Originals);
+addCards(snackBox, Snacks);
+addCards(sweetyBox, Sweeties);
+addCards(complimentaryBox, Complimentaries);
---
+++ meal-data.js
@@ -0,0 +1,219 @@
+//Meal Database
+let Appetizers = [
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish2<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish3<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish4<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+
+];
+let MainCourses = [
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish2<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish3<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish4<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+
+];
+let Westerns = [
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish2<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish3<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish4<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML: '2 Days Storage<br>Cold and Dry Places',
+    },
+
+];
+let Salads = [
+     {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish2<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish3<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+    {
+        imgPath: "./images/v220_95.png",
+        nameHTML: 'Steamed Fish4<br>with Curry Paste',
+        nameThai: 'ห่อหมกทรงเครื่อง ',
+        price: 80,
+        ingredientHTML: "PumpkinsTest,<br>MilkTest,<br>ButterTest,<br>WhipcreamTest",
+        description: 'Pumpkin is hidden missing piece to add sweet and mild taste in your favorite cupcakes.Now, let’s grab this and enjoy more good to both your health and joy.',
+        preservationHTML:'2 Days Storage<br>Cold and Dry Places',
+    },
+
+]
+
+// Element Creater================
+const addCards = function (box, Foods) {
+    for (food of Foods) {
+        const cardSmall = document.createElement('div');
+        const foodImg = document.createElement('img');
+        const cardText = document.createElement('div');
+        const foodNameHTML = document.createElement('p');
+        const foodNameThai = document.createElement('span');
+        const foodPrice = document.createElement('span');
+        const ingredientHTML = document.createElement('div');
+        const foodDescription = document.createElement('div');
+        const preservationHTML = document.createElement('div');
+
+        cardSmall.classList.add("card-small", "flex-shrink-0", "mx-2");
+        cardSmall.setAttribute("data-bs-toggle", "modal");
+        cardSmall.setAttribute("data-bs-target", "#modal-bakery");
+        foodImg.classList.add("d-block", "card-img-small");
+        foodImg.setAttribute("src", food.imgPath);
+        cardText.classList.add("card-text", "w-100", "h-auto", "pt-1", "ps-1");
+
+        if (box == appetizerBox || box == mainCourseBox) {
+            foodNameHTML.classList.add("card-name-small", "mb-md-1");
+            foodNameThai.classList.add("card-name-thai-small", "text-wrap", "float-start");
+            foodPrice.classList.add("card-price-small", "float-end");
+        } else {
+            foodNameHTML.classList.add("card-name-small-white", "mb-md-1");
+            foodNameThai.classList.add("card-name-thai-small-white", "text-wrap", "float-start");
+            foodPrice.classList.add("card-price-small-white", "float-end");
+        }
+        foodNameHTML.innerHTML = food.nameHTML;
+        foodNameThai.innerText = food.nameThai;
+        foodPrice.innerText = food.price + ",-";
+        ingredientHTML.classList.add("ingredient", "d-none");
+        ingredientHTML.innerHTML = food.ingredientHTML;
+        foodDescription.classList.add("productDescription", "d-none");
+        foodDescription.innerText = food.description;
+        preservationHTML.classList.add("preservation", "d-none");
+        preservationHTML.innerHTML = food.preservationHTML;
+
+        cardText.appendChild(foodNameHTML);
+        cardText.appendChild(foodNameThai);
+        cardText.appendChild(foodPrice);
+        cardSmall.appendChild(foodImg);
+        cardSmall.appendChild(cardText);
+        cardSmall.appendChild(ingredientHTML);
+        cardSmall.appendChild(foodDescription);
+        cardSmall.appendChild(preservationHTML);
+
+        box.appendChild(cardSmall);
+    }
+};
+
+const appetizerBox = document.querySelector('#appetizerBox');
+const mainCourseBox = document.querySelector('#mainCourseBox');
+const westernBox = document.querySelector('#westernBox');
+const saladBox = document.querySelector('#saladBox');
+
+addCards(appetizerBox, Appetizers);
+addCards(mainCourseBox, MainCourses);
+addCards(westernBox, Westerns);
+addCards(saladBox, Salads);
