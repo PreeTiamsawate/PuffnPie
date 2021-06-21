ccbe913f3e1b439a85b4e4fba29f07bc82e39969
Pree Tiamsawate <preetiamsawate@gmail.com>
2021-06-21 18:48:32 +0700

    improve mobile navbar

--- navbar-control.js
+++ navbar-control.js
@@ -1,9 +1,22 @@
 const currentLocation = location.href;
-        const navLinks = document.querySelectorAll('.nav-link');
-        for (navLink of navLinks) {
-            if (navLink.href === currentLocation) {
-                navLink.classList.add('nav-menu-link-acitve');
-                navLink.classList.remove('nav-menu-link');
-                navLink.setAttribute("aria-current", "page");
-            }
-        }
+const navLinks = document.querySelectorAll('.nav-link');
+const mobileLinks =document.querySelectorAll('.mobile-link')
+for (navLink of navLinks) {
+    if (navLink.href === currentLocation) {
+        navLink.classList.add('nav-menu-link-acitve');
+        navLink.classList.remove('nav-menu-link');
+        navLink.setAttribute("aria-current", "page");
+    }
+}
+for (mobileLink of mobileLinks) {
+    if (mobileLink.href === currentLocation) {
+        mobileLink.children[0].classList.add('d-none');
+        mobileLink.children[0].classList.remove('d-block');
+        mobileLink.children[1].classList.add('d-block');
+        mobileLink.children[1].classList.remove('d-none');
+        mobileLink.children[2].classList.remove('textIcon');
+        mobileLink.children[2].classList.add('textIcon-active');
+
+
+    }
+}
